23f34870919788c91e66d8376c72f350502e7d50
ALEX PARSHIKOV <parahumanoid2@gmail.com>
2024-06-06 10:20:01 +0300

    task-1 test

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+makeTransaction(quantity, pricePerDroid, customerCredits){
+    let cost = quantity * pricePerDroid;
+    if (cost > customerCredits) return "Insufficient funds!";
+    else return `You ordered ${quantity} droids worth ${cost} credits!`;
+}
